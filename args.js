725220a1bf8d69460e9c99462cc215ed580f7708
manataliekeithse <141150584+manataliekeithse@users.noreply.github.com>
2024-09-09 19:48:41 +0800

    save

---
+++ args.js
@@ -0,0 +1,4 @@
+console.log("Arguments:", process.argv);
+
+//run the following command
+//node args.js arg1 arg2 arg3
---
+++ index.js
@@ -0,0 +1,5 @@
+global.console.log("Hello World"); //explicit console log from the global object
+console.log("Hello World (2)!"); //implicit console log without showing that the  function is from the global object
+
+console.log("The file name of this file is: ", __filename);
+console.log("The directory from where the file is located is: ", __dirname);
